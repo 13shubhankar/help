b36b59e0703ca121bd47888ea9375565e02443d1
13shubhankar <shubhankarraj13@gmail.com>
2025-12-19 01:49:40 +0530

    commit

---
+++ loginapp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class loginapp {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter username: ");
+String username = sc.nextLine();
+System.out.print("Enter password: ");
+String password = sc.nextLine();
+if(username.equals("admin") && password.equals("1234")) {
+System.out.println("Login Successful");
+} else {
+System.out.println("Access Denied");
+}
+sc.close();
+}
+}
